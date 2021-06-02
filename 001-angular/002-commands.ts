98fed0bbafb8a1014b0a5bcdf00ba33b1ffd16ce
tarun <tarun.verma151@gmail.com>
2021-06-02 20:46:52 +0530

    angular cli commands

--- 001-angular/002-commands.ts
+++ 001-angular/002-commands.ts
@@ -2,7 +2,7 @@
  * 
  *  Angular CLI:
  *  ===========
- *  -   Install Angular CLI :
+ *  -   Install Angular CLI globally :
  *          npm install -g @angular/cli
  *  -   Once installed, we can use commands with 'ng'
  *  -   Ex : To run angular app :
@@ -10,8 +10,33 @@
  * 
  *  Commands:
  *  ========
+ *  -   General syntax
+ *          ng <command> <args> --<options>
  *  -   Create new angular application:
  *          ng new apm-new --prefix pm
- * 
- *  -   
+ *  -   Generate 
+ *      -   Component:      ng g c product/product-detail --flat
+ *          --flat : doesn't create new folder.
+ *      -   Route Guard :   ng g g product/product-detail
+ *      -   Module:         ng g m product/product --flat -m app
+ *          -m  : import the module in app module.
+ *                          ng g m shared/shared --flat -m product/product.module 
+ *      -   Directives :    ng g d <name>
+ *      -   Interfaces :    ng g i <name>
+ *      -   Pipes :         ng g p <name>
+ *      -   Services:       ng g s <name>
+ *  -   Run application:
+ *              ng serve
+ *      -   compiles the application
+ *      -   generate application bundles
+ *      -   starts a local web server
+ *      -   serves the application from memory
+ *      -   rebuilds on file changes.
+ *  -   Testing the application:
+ *      -   ng test
+ *          ng e2e      // for end to end testing
+ *  -   Deploy the application
+ *          ng deploy
+ *  -   version upgrade
+ *          ng update
  */
