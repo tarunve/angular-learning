bcf2689004e89763aece79730ce4adc8447f0c8c
tarun <tarun.verma151@gmail.com>
2021-07-03 19:05:43 +0530

    adding express server in angular project

---
+++ p002-angular-chrome-debugger/server.ts
@@ -0,0 +1,64 @@
+import express from 'express';
+import * as PKG from './package.json';
+import { join } from 'path';
+import helmet from 'helmet';
+import { json , urlencoded } from 'body-parser';
+import * as expressHandlebars from 'express-handlebars';
+import compression from 'compression';
+import { readFileSync } from 'fs';
+
+const APP = express();
+const LOCAL_PORT = 8080;
+const PORT = process.env.PORT || LOCAL_PORT;
+const PATH_STATIC_PKG_ROUTE = `/pkg/${PKG.version}/web`;
+const DOCUMENT_ROOT = join(__dirname, 'production');
+
+const staticPath = function(pkg){
+    return function(req, res, next) : void {
+        req.myPath = `pkg/${pkg.version}/web`
+        next();
+    }
+}
+
+APP.use(staticPath(PKG));
+APP.use(helmet());
+APP.use(compression());
+APP.use(urlencoded({ extended : false}));
+APP.use(json());
+
+APP.use(
+    PATH_STATIC_PKG_ROUTE,
+    express.static(DOCUMENT_ROOT, {
+        maxAge: '1y',
+        fallthrough: false
+    })
+);
+
+APP.engine(
+    'html',
+    expressHandlebars({
+        layoutsDir: DOCUMENT_ROOT,
+        helpers: {
+            toJson: JSON.stringify
+        }
+    })
+);
+
+APP.get('/', function(req, res){
+        res.send(readFileSync(join(
+            DOCUMENT_ROOT,
+            'index.html'
+        ),
+        {
+            encoding: 'utf8'
+        }
+    )
+    .replace(/{STATIC_PATH}/, `pkg/${PKG.version}/web`)
+    )
+})
+
+APP.listen(PORT, () => {
+    console.info(`Node application ${PKG.name} has started their node server on port ${PORT}`);
+});
+
+process.on('SIGINT', () => process.exit(1));
