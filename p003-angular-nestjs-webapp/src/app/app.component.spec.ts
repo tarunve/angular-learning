663ee41d7dcc9711ec416b7a66799bd55b2f1dfc
tarun <tarun.verma151@gmail.com>
2021-07-28 10:56:08 +0530

    app component fix

--- p003-angular-nestjs-webapp/src/app/app.component.spec.ts
+++ p003-angular-nestjs-webapp/src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'p003-angular-nestjs-webapp'`, () => {
+  it(`should have as title 'Angular-NestJS Webapp`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('p003-angular-nestjs-webapp');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('p003-angular-nestjs-webapp app is running!');
+    expect(app.title).toEqual('Angular-NestJS Webapp');
   });
 });
--- p003-angular-nestjs-webapp/src/app/app.component.ts
+++ p003-angular-nestjs-webapp/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'p003-angular-nestjs-webapp';
+  title = 'Angular-NestJS Webapp';
 }
