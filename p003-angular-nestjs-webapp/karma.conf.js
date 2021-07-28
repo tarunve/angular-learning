74293973ab49df9507f7bedb53a9b86f38c88b98
tarun <tarun.verma151@gmail.com>
2021-07-28 10:48:31 +0530

    change test report location

--- p003-angular-nestjs-webapp/karma.conf.js
+++ p003-angular-nestjs-webapp/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/p003-angular-nestjs-webapp'),
+      dir: require('path').join(__dirname, './reports/coverage'),
       subdir: '.',
       reporters: [
         { type: 'html' },
